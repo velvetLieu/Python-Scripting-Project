9afe155de6c55888805355c6be47d248f2d724bd
Jonathan Lieu <razerlieu@gmail.com>
2022-11-09 15:42:37 -0800

    implement get_name_from_paths
    function to strip off "game" from file name directory

--- get_game_data.py
+++ get_game_data.py
@@ -19,13 +19,30 @@ def find_all_game_paths(source):
         break
     return game_paths
 
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+
+    return new_names
+
+def create_dir(path):
+    if not os.path.exists:
+        os.mkdir(path)
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    print(game_paths)
+    new_game_dirs = get_name_from_paths(game_paths, "game")
+
+    print(new_game_dirs)
+
+    create_dir(target_path)
 
 if __name__ == "__main__":
     args = sys.argv
