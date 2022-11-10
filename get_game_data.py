9517cf13b2cd563f7351234c08dff29b424c74a8
Jonathan Lieu <razerlieu@gmail.com>
2022-11-09 16:18:24 -0800

    implement compile_game_code

--- get_game_data.py
+++ get_game_data.py
@@ -6,6 +6,8 @@ import sys
 
 
 GAME_DIR_PATTERN = "game"
+GAME_CODE_EXTENSION = ".go"
+GAME_COMPILE_COMMAND = ["go", "build"]
 
 def find_all_game_paths(source):
     game_paths = []
@@ -45,6 +47,31 @@ def make_json_metadata_file(path, game_dirs):
     with open(path, "w") as f:
         json.dump(data, f)
 
+def compile_game_code(path):
+    code_file_name = None
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith(GAME_CODE_EXTENSION):
+                code_file_name = file
+                break
+
+        break
+
+    if code_file_name is None:
+        return
+
+    command = GAME_COMPILE_COMMAND + [code_file_name]
+    run_command(command, path)
+
+def run_command(command, path):
+    cwd = os.getcwd()
+    os.chdir(path)
+
+    result = run(command, stdout=PIPE, stdin=PIPE, universal_newlines=True)
+    print("compile result ", result)
+
+    os.chdir(cwd)
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
@@ -58,6 +85,10 @@ def main(source, target):
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
+        compile_game_code(dest_path)
+
+    json_path = os.path.join(target_path, "metadata.json")
+    make_json_metadata_file(json_path, new_game_dirs)
 
 if __name__ == "__main__":
     args = sys.argv
