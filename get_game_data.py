1ce1757c37c0eff12a7ccf15a707b4ff05702829
Jonathan Lieu <razerlieu@gmail.com>
2022-11-09 16:00:00 -0800

    Update get_game_data.py

--- get_game_data.py
+++ get_game_data.py
@@ -32,17 +32,32 @@ def create_dir(path):
     if not os.path.exists:
         os.mkdir(path)
 
+def copy_and_overwrite(source, destination):
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    shutil.copytree(source, destination)
+
+def make_json_metadata_file(path, game_dirs):
+    data = {
+        "gameNames": game_dirs,
+        "numberOfGames": len(game_dirs)
+    }
+    with open(path, "w") as f:
+        json.dump(data, f)
+
 def main(source, target):
     cwd = os.getcwd()
     source_path = os.path.join(cwd, source)
     target_path = os.path.join(cwd, target)
 
     game_paths = find_all_game_paths(source_path)
-    new_game_dirs = get_name_from_paths(game_paths, "game")
-
-    print(new_game_dirs)
+    new_game_dirs = get_name_from_paths(game_paths, "_game")
 
     create_dir(target_path)
+
+    for src, dest in zip(game_paths, new_game_dirs):
+        dest_path = os.path.join(target_path, dest)
+        copy_and_overwrite(src, dest_path)
 
 if __name__ == "__main__":
     args = sys.argv
